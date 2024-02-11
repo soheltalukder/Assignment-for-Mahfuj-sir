01bbc141cb8542c2bc80294fa6b17b3c75c432a8
soheltalukder <mssohel.freelancer@gmail.com>
2024-02-12 00:58:55 +0600

    Sohel

---
+++ resources/views/student/create_student.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container ">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Create Student') }}</div>
+
+                <div class="card-body ">
+                    <div class="container">
+                        <h2 class="mt-5 mb-4 text-center fw-bold text-primary">Student's Data</h2>
+                        <form action="{{route('store.student')}}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Full Name:</label>
+                                <input type="text" class="form-control" name="name" placeholder="Enter your name"
+                                    required>
+                            </div>
+                            <div class="form-group">
+                                <label for="roll"> Class Roll:</label>
+                                <input type="text" class="form-control" name="roll" placeholder="Enter your roll number"
+                                    required>
+                            </div>
+                            <div class="form-group">
+                                <label for="age">Age:</label>
+                                <input type="number" class="form-control" name="age" placeholder="Enter your age"
+                                    required>
+                            </div>
+                            <div class="form-group">
+                                <label for="gender">Gender:</label>
+                                <select class="form-control" name="gender" required>
+                                    <option value="">Select</option>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">Phone Number:</label>
+                                <input type="tel" class="form-control" name="phone"
+                                    placeholder="Enter your phone number" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="district"> Home District:</label>
+                                <input type="text" class="form-control" name="district"
+                                    placeholder="Enter your district" required>
+                            </div>
+                            <br>
+                            <button type="submit" class="btn btn-info">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('footerScript')
+@if(Session::has('success'))
+<script type="text/javascript">
+$(function() {
+    toastr.success("{{ Session::get('success') }}");
+})
+</script>
+@endif
+@if(Session::has('fail'))
+<script type="text/javascript">
+$(function() {
+    toastr.error("{{ Session::get('fail') }}");
+})
+</script>
+@endif
+@stop
---
+++ resources/views/student/student_edit.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center bg-info">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Update Student Data') }}</div>
+
+                <div class="card-body">
+                    <div class="container">
+                        <h2 class="mt-5 mb-4 fw-bold">Update Student Data</h2>
+                        <form action="{{route('student.update',$id)}}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <label for="name">Ful Name:</label>
+                                <input type="text" class="form-control" name="name" value="{{$student->name}}" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="roll">Class Roll:</label>
+                                <input type="text" class="form-control" name="roll" value="{{$student->roll}}" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="age">Age:</label>
+                                <input type="number" class="form-control" name="age" value="{{$student->age}}" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="gender">Gender:</label>
+                                <select class="form-control" name="gender" required>
+                                    <option value="{{$student->gender}}">{{$student->gender}}</option>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">Phone Number:</label>
+                                <input type="tel" class="form-control" name="phone" value="{{$student->phone}}"
+                                    required>
+                            </div>
+                            <div class="form-group">
+                                <label for="district">Home District:</label>
+                                <input type="text" class="form-control" name="district" value="{{$student->district}}"
+                                    required>
+                            </div>
+                            <button type="submit" class="btn btn-info">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('footerScript')
+@if(Session::has('success'))
+<script type="text/javascript">
+$(function() {
+    toastr.success("{{ Session::get('success') }}");
+})
+</script>
+@endif
+@if(Session::has('fail'))
+<script type="text/javascript">
+$(function() {
+    toastr.error("{{ Session::get('fail') }}");
+})
+</script>
+@endif
+@stop
